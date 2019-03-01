6613ab525be7ddd8be718e49a58c13d837f1ded9
MisterBigThor <vcr9713@gmail.com>
2019-03-01 19:09:28 +0000

    first commit

---
+++ sys.c
@@ -0,0 +1,57 @@
+/*
+ * sys.c - Syscalls implementation
+ */
+#include <devices.h>
+
+#include <utils.h>
+
+#include <io.h>
+
+#include <mm.h>
+
+#include <mm_address.h>
+
+#include <sched.h>
+
+#define LECTURA 0
+#define ESCRIPTURA 1
+
+
+int check_fd(int fd, int permissions)
+{
+  if (fd!=1) return -9; /*EBADF*/
+  if (permissions!=ESCRIPTURA) return -13; /*EACCES*/
+  return 0;
+}
+
+int sys_ni_syscall()
+{
+	//AQUI SE LLEGARA SI NO ESTA DEFINIDA LA SYSCALL
+	return -38; /*ENOSYS*/	
+}
+
+int sys_getpid()
+{
+	return current()->PID;
+}
+
+int sys_fork()
+{
+  int PID=-1;
+
+  // creates the child process
+  
+  return PID;
+}
+
+int sys_write(int fd, char * buffer, int size){
+
+	if(check_fd(fd, ESCRIPTURA) != 0) return check_fd(fd, ESCRIPTURA);
+	if(* buffer == NULL) return -45; /*Buffer a null*/
+	if(size < 0) return -14; /*TAMAÑO NEGATIVO*/
+	else return sys_write_console(buffer, size);
+}
+
+void sys_exit()
+{  
+}
