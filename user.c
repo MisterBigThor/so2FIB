c84cb12de453956e9c95b6af4d135d2a39f1b1cd
MisterBigThor <vcr9713@gmail.com>
2019-04-29 10:55:01 +0000

    user.c

--- user.c
+++ user.c
@@ -10,13 +10,13 @@ main(void)
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
 	/* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
-	write(1, "Escrito con write \n", strlen("Escrito con write \n"));
-	writefast(1,"Escrito con write fast \n",strlen("Escrito con write fast \n"));	
-	itoa(getpid(), buff);
-	write(1, "PID ACTUAL: ", strlen("PID ACTUAL: "));
-	writefast(1, buff, strlen(buff));
+//	write(1, "Escrito con write \n", strlen("Escrito con write \n"));
+	//writefast(1,"Escrito con write fast \n",strlen("Escrito con write fast \n"));	
+	//itoa(getpid(), buff);
+	//write(1, "PID ACTUAL: ", strlen("PID ACTUAL: "));
+	//writefast(1, buff, strlen(buff));
 
-	//runjp();
+	runjp();
 
 	//runjp_rank(0,0);
 
